54594caea65f265d3ee7e5df9e321095068abe17
Bartosz Mazurkiewicz <bmazurki@geo.tuwien.ac.at>
2020-04-09 16:14:05 +0300

    Initial commit

---
+++ src/enigma_devices/enigma_plug_board.py
@@ -0,0 +1,9 @@
+# This is an enhanced version of th enigma device with a plug board which increases dramatically the number of setting possibilities
+
+
+class enigma_simple:
+    def __init__(self, rotor_1, rotor_2, rotor_3, reflector):
+        pass
+
+
+class
